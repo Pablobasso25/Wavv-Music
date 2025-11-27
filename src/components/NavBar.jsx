7b8bbdd64a44a564f2df643d08e0cd550aa98ce9
AlvaroMorillo <alvaromorillo28@gmail.com>
2025-11-27 01:41:16 -0300

    feat: agregar maquetacion de barra de busqueda para las canciones

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,26 +5,16 @@ const NavBar = () => {
   return (
     <Navbar>
       <Container>
-        <Navbar.Brand>
-          Wavv
-        </Navbar.Brand>
+        <Navbar.Brand>Wavv</Navbar.Brand>
 
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
 
         <NavBar.Collapse>
           <Nav>
-            <NavLink>
-              Home
-            </NavLink>
-            <NavLink>
-              Playlist
-            </NavLink>
-            <NavLink>
-              NOSOTROS
-            </NavLink>
-            <NavLink>
-              ADMIN
-            </NavLink>
+            <NavLink>Home</NavLink>
+            <NavLink>Playlist</NavLink>
+            <NavLink>NOSOTROS</NavLink>
+            <NavLink>ADMIN</NavLink>
           </Nav>
           <div>
             <div>
@@ -32,6 +22,34 @@ const NavBar = () => {
                 <span>Buscando...</span>
               </div>
             </div>
+          </div>
+
+          <Form.Control type="text" placeholder="Buscar canciones..." />
+          <div>
+            <div>
+              <div>
+                <span>Buscando...</span>
+              </div>
+              Buscando...
+            </div>
+            <div>
+              <small>resultados</small>
+            </div>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <h6></h6>
+                <small></small>
+              </div>
+              <div>
+                <i></i>
+                <i></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <i></i>
+            No se encontraron resultados
           </div>
         </NavBar.Collapse>
       </Container>
