1799158d6b9c8ca44f0e4eaae52bde40822c97e2
RominaDanelutto <r.danelutto@gmail.com>
2025-11-28 19:05:21 -0300

    feat(about-us): agrega imports de imágenes y dependencias iniciales del componente

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import TeamMember from "./TeamMember";
+import logo from "../logo.png";
+import pabloImg from "../aboutUs/pablo.jpg";
+import alvaroImg from "../aboutUs/alvaro.jpg";
+import rominaImg from "../aboutUs/romina.jpg";
+import juanImg from "../aboutUs/juan.png";
+import patricioImg from "../aboutUs/patricio.png";
