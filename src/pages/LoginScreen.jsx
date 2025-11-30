8de50239d14d992851d64634e0c0a8dd0fc60009
Patricio Manuel Romero Celiz <patorceliz05@gmail.com>
2025-11-30 13:20:32 -0300

    feat: crear formulario de registro

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -10,3 +10,14 @@ import {
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+
+const LoginScreen = () => {
+  const { login } = useAuth();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }};
