6bbdd58079ffc30839e147eacdff8e13815f4462
Pablo Basso <pablobasso25@gmail.com>
2025-11-30 00:53:25 -0300

    feat: Importar estilos de Bootstrap y librerías de iconos globales

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "bootstrap/dist/css/bootstrap.min.css"; 
+import "bootstrap-icons/font/bootstrap-icons.css"; 
+import "boxicons/css/boxicons.min.css";
 import './index.css'
 import App from './App.jsx'
 
